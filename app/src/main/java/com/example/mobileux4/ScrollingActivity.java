acc5d445521ded1f0fc1bff862c47330a45bcf64
Matthew Chan <mchanjh1@gmail.com>
2021-12-28 17:11:22 +0800

    Media player

--- app/src/main/java/com/example/mobileux4/ScrollingActivity.java
+++ app/src/main/java/com/example/mobileux4/ScrollingActivity.java
@@ -1,5 +1,6 @@
 package com.example.mobileux4;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
@@ -12,15 +13,20 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.mobileux4.databinding.ActivityScrollingBinding;
 
-public class ScrollingActivity extends AppCompatActivity {
+public class ScrollingActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     private ActivityScrollingBinding binding;
-
+    MediaPlayer player = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
 
         binding = ActivityScrollingBinding.inflate(getLayoutInflater());
@@ -30,8 +36,26 @@ public class ScrollingActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
         toolBarLayout.setTitle(getTitle());
+        Spinner spinner = findViewById(R.id.playback_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.playback_speed, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
 
         FloatingActionButton fab = binding.fab;
+        if (player == null) {
+            player = MediaPlayer.create(this, R.raw.bgm);
+
+            player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    stopPlayer();
+                }
+            });
+        }
+
+        player.start();
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -40,7 +64,32 @@ public class ScrollingActivity extends AppCompatActivity {
             }
         });
     }
+    private void stopPlayer() {
+        if (player != null) {
+            player.release();
+            player = null;
+            Toast.makeText(this, "MediaPlayer released", Toast.LENGTH_SHORT).show();
+        }
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        System.out.println(text);
+        if(text.equals("1x")){
+            player.setPlaybackParams(player.getPlaybackParams().setSpeed(1f));
+        }else if (text.equals("2x")){
+            player.setPlaybackParams(player.getPlaybackParams().setSpeed(2f));
+        }else if (text.equals("4x")){
+            player.setPlaybackParams(player.getPlaybackParams().setSpeed(4f));
+        }else if (text.equals("0.5x")){
+            player.setPlaybackParams(player.getPlaybackParams().setSpeed(0.5f));
+        }
+    }
 
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
